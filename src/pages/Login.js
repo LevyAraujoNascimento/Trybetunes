5e21342a003120fa9751af438b2bd62ae87ffbe2
Levy Araujo <levy4raujo@gmail.com>
2023-01-18 14:14:13 -0300

    2º requisito em andamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    startButton: true,
+    loading: false,
+    redirect: false,
+  };
+
+  startTest = ({ target }) => {
+    const limit = 3;
+    const name = target.value;
+    this.setState(() => ({
+      name,
+    }));
+    if (name.length >= limit) {
+      this.setState(() => ({
+        startButton: false,
+      }));
+    } else {
+      this.setState(() => ({
+        startButton: true,
+      }));
+    }
+  };
+
+  saveUser = async () => {
+    const { name } = this.state;
+    const user = {
+      name,
+    };
+    this.setState(
+      { loading: true },
+      async () => {
+        await createUser(user);
+        this.setState({
+          loading: false,
+          redirect: true,
+        });
+      },
+    );
+  };
+
   render() {
+    const { startButton, loading, redirect } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <form data-testid="page-login">
+        <fieldset>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            onChange={ this.startTest }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ startButton }
+            onClick={ this.saveUser }
+          >
+            Entrar
+          </button>
+        </fieldset>
+        { loading ? <Loading /> : ''}
+        { redirect ? <Redirect to="search" /> : ''}
+      </form>
     );
   }
 }
